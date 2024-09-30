2985b089e11327ccad7f665f1040af2bd29c7e6f
skyfourhmmm <nguyenthientu413.com>
2024-09-30 20:02:07 +0700

    update_minh_chung

--- React_Native/Expo_Tuan5_Navigation/App.js
+++ React_Native/Expo_Tuan5_Navigation/App.js
@@ -1,24 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // or any files within the Snack
-import Home from './components/Home';
-import Color from './components/Color';
+import Home from "./components/Home";
+import Color from "./components/Color";
 
 export default function App() {
-
-const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Color">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Color" component={Color} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
